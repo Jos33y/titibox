1f32f6776ce889315c30856cd23768beb27f051a
Jos33y <josephlagbalu@gmail.com>
2020-02-19 10:49:32 +0100

    added all files

---
+++ admin_area/includes/dbcon.php
@@ -0,0 +1,11 @@
+<?php
+
+//$con = mysqli_connect("localhost", "Joseey", "", "ecom_store_doris");
+
+$con = mysqli_connect("localhost", "u721127448_Titi", "LagbaluJ19", "u721127448_titibox");
+
+//$conn = mysqli_connect("localhost", "Joseey", "", "eacez_db");
+
+$conn = mysqli_connect("localhost", "u721127448_Admin", "LagbaluJ19", "u721127448_eacez");
+
+?>
