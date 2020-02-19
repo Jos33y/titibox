1d7dc2356d212debf8635287fda2f90daa2b146b
Jos33y <josephlagbalu@gmail.com>
2020-02-19 12:34:56 +0100

    corrected header

--- admin_area/includes/header.php
+++ admin_area/includes/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>OT -Tech admin area</title>
+    <title>Titi Box admin area</title>
     <link rel="stylesheet" href="css/bootstrap-337.min.css">
    <link rel="stylesheet" href="font-awsome\css\font-awesome.min.css">
    <link rel="stylesheet" href="css/styles.css">
